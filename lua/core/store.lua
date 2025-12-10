1d9728200606e39f6f0ca381526b7b32af6e5fe6
wancup <contact@wancup.dev>
2025-12-11 02:29:04 +0900

    fix: add missing buf to store list

--- lua/core/store.lua
+++ lua/core/store.lua
@@ -18,7 +18,13 @@ M.cycle_progress_win = nil
 ---@param bufnr integer
 ---@return boolean
 M.is_own_buf = function(bufnr)
-	local all = { M.buffer_list_win, M.buffer_summary_win, M.cycle_list_win, M.cycle_preview_win }
+	local all = {
+		M.buffer_list_win,
+		M.buffer_summary_win,
+		M.cycle_list_win,
+		M.cycle_preview_win,
+		M.cycle_progress_win,
+	}
 	for _, win in pairs(all) do
 		if win ~= nil and vim.api.nvim_win_get_buf(win) == bufnr then
 			return true
