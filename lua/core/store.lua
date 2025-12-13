5a1d93905fdc09c3dbeb836cfc050e1c699055d5
wancup <contact@wancup.dev>
2025-12-13 22:27:10 +0900

    fix: error on closing wins manually

--- lua/core/store.lua
+++ lua/core/store.lua
@@ -18,17 +18,26 @@ M.cycle_progress_win = nil
 ---@param bufnr integer
 ---@return boolean
 M.is_own_buf = function(bufnr)
-	local all = {
-		M.buffer_list_win,
-		M.buffer_summary_win,
-		M.cycle_list_win,
-		M.cycle_preview_win,
-		M.cycle_progress_win,
+	local fin_fields = {
+		"buffer_list_win",
+		"buffer_summary_win",
+		"cycle_list_win",
+		"cycle_preview_win",
+		"cycle_progress_win",
 	}
-	for _, win in pairs(all) do
-		if win ~= nil and vim.api.nvim_win_get_buf(win) == bufnr then
+	for _, field in pairs(fin_fields) do
+		local win = M[field]
+		if win == nil then
+			goto continue
+		end
+		if not vim.api.nvim_win_is_valid(win) then
+			M[field] = nil
+			goto continue
+		end
+		if vim.api.nvim_win_get_buf(win) == bufnr then
 			return true
 		end
+		::continue::
 	end
 	return false
 end
